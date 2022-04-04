8c5e8df5135e301d5099c7ebc132ea958de6c09d
Jeffrey Jernstrom <jeff@pop-os.localdomain>
2022-04-03 18:40:17 -0700

    removed docker file, cleaned up repo, and added test infrastructure

--- src/app.js
+++
@@ -1,7 +0,0 @@
-// Calculate the factorial of the number n recursively
-function factorialRec(n) {
-    if (n == 1)
-        return 1;
-    return n * factorialRec(n - 1);
-}
-console.log(factorialRec(5));
---
+++ test/factorial.test.ts
@@ -0,0 +1,5 @@
+import { exportAllDeclaration } from "@babel/types"
+
+test('one + one is equal to two', () => {
+    expect(1 + 1).toBe(2);
+})
